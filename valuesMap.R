58cfa65e4a809bcaa67111dadc280afcd1f09caa
Alexander Titov <nevtiz@gmail.com>
2020-05-11 04:01:22 +0300

    Russian labels fixed

--- valuesMap.R
+++ valuesMap.R
@@ -42,19 +42,6 @@ ggplot(data = NMDS, aes(MDS1*10, MDS2*10)) +
   #stat_ellipse(geom = "polygon", alpha = 0.5, aes(fill = as.factor(profile))) +
   geom_mark_hull(aes(fill = as.factor(profile)), alpha = 0.08, colour = NA,
                  expand = unit(3, "mm"), 
-<<<<<<< HEAD
-                 concavity = 1) +
-  #geom_encircle(alpha = 0.5, aes(fill = as.factor(profile))) +
-  geom_point(aes(color = as.factor(profile)), size = 3) +
-  geom_segment(data=vec.sp.df,
-               aes(x = 0,xend = MDS1*1.5,y = 0, yend= MDS2*1.5),
-               lineend = "butt",
-               linejoin = "mitre",
-               size = 0.5, 
-               arrow = arrow(length = unit(0.2, "cm")),
-               colour="grey"
-  ) + 
-=======
                   concavity = 1) +
   #geom_encircle(alpha = 0.5, aes(fill = as.factor(profile))) +
   geom_point(aes(color = as.factor(profile)), size = 3) +
@@ -66,7 +53,6 @@ ggplot(data = NMDS, aes(MDS1*10, MDS2*10)) +
     arrow = arrow(length = unit(0.2, "cm")),
     colour="grey"
     ) + 
->>>>>>> 1c7ad456b8f16d86ab265b39c3ff263daf5abbf3
   geom_label_repel(aes(label = dataValues$label), label.size = NA, fill = NA) + 
   #geom_polygon(aes(color = as.factor(profile)), alpha = 0.1) +
   geom_text_repel(data = vec.sp.df,aes(x = MDS1*1.6, y = MDS2*1.6, label = species),size = 5) +
